487584faec100b49e34f81a4a89c1aba695ab4e2
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-02-01 21:31:15 -0600

    functional cdk stack

--- bin/aws-cdk-aurora-cw-alarms.ts
+++ bin/aws-cdk-aurora-cw-alarms.ts
@@ -5,17 +5,8 @@ import { AwsCdkAuroraCwAlarmsStack } from '../lib/aws-cdk-aurora-cw-alarms-stack
 
 const app = new cdk.App();
 new AwsCdkAuroraCwAlarmsStack(app, 'AwsCdkAuroraCwAlarmsStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  dbName: process.env.DB_NAME!,
+  dbUsername: process.env.DB_USERNAME!,
+  dbPassword: process.env.DB_PASSWORD!,
+  safeIp: process.env.SAFE_IP!
 });
--- lib/aws-cdk-aurora-cw-alarms-stack.ts
+++ lib/aws-cdk-aurora-cw-alarms-stack.ts
@@ -1,16 +1,118 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput, Duration, SecretValue } from 'aws-cdk-lib';
+import * as rds from "aws-cdk-lib/aws-rds";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+
+export interface AwsCdkAutoraCwAlarmsStackProps extends StackProps {
+  readonly safeIp: string;
+  readonly dbUsername: string;
+  readonly dbPassword: string;
+  readonly dbName: string;
+}
 
 export class AwsCdkAuroraCwAlarmsStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: AwsCdkAutoraCwAlarmsStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC with 2 public subnets across two availability zone
+    const vpc = new ec2.Vpc(this, 'DbVpc', {
+      cidr: '10.0.0.0/20',
+      maxAzs: 2,
+      natGateways: 0
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkAuroraCwAlarmsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Security group to allow connects to database from my home IP
+    const dbSecurityGroup = new ec2.SecurityGroup(this, 'DbSecurityGroup', {
+      vpc,
+      description: 'Aurora Postgres Security Group',
+      allowAllOutbound: true
+    });
+    dbSecurityGroup.addIngressRule(
+      ec2.Peer.ipv4(props.safeIp),
+      ec2.Port.tcp(5432),
+      'Allow TCP/Postgres Connection from SafeIP on Port 5432'
+    );
+
+
+    // Aurora Postgres Database Cluster of 1 Writer and 1 Reader
+    const dbCluster = new rds.DatabaseCluster(this, 'DbCluster', {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({
+        version: rds.AuroraPostgresEngineVersion.VER_13_4
+      }),
+      credentials: rds.Credentials.fromPassword(
+        props.dbUsername,
+        SecretValue.plainText(props.dbPassword),
+      ),
+      defaultDatabaseName: props.dbName,
+      instances: 2, // one writer and one reader
+      instanceProps: {
+        vpc,
+        publiclyAccessible: true,
+        securityGroups: [ dbSecurityGroup ],
+        vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC }
+      }
+    });
+
+    // CPU Utilization CloudWatch Metric Averaged over 2 mins
+    // for DB Cluster, Writer Instance and Reader Instance
+    const dbCpuMetric = dbCluster.metricCPUUtilization({
+      period: Duration.minutes(2)
+    });
+    const dbWriterCpuMetric = dbCluster.metricCPUUtilization({
+      period: Duration.minutes(2),
+      dimensionsMap: { Role: 'WRITER' }
+    });
+    const dbReaderCpuMetric = dbCluster.metricCPUUtilization({
+      period: Duration.minutes(2),
+      dimensionsMap: { Role: 'READER' }
+    });
+
+    // CloudWatch alarms for CPU Utilization metrics set to
+    // alarm state when CPU os over 30 percent at
+    // DB Cluster, Writer and Reader instances
+    const dbCpuAlarm = dbCpuMetric.createAlarm(this, 'DbCpuAlarm', {
+      evaluationPeriods: 1,
+      alarmDescription: "Cluster CPU Over 30 Percent",
+      threshold: 30
+    });
+    const dbWriterCpuAlarm = dbWriterCpuMetric.createAlarm(this, 'DbWriterCpuAlarm', {
+      evaluationPeriods: 1,
+      alarmDescription: "Writer CPU Over 30 Percent",
+      threshold: 30
+    });
+    const dbReaderCpuAlarm = dbReaderCpuMetric.createAlarm(this, 'DbReaderCpuAlarm', {
+      evaluationPeriods: 1,
+      alarmDescription: "Reader CPU Over 30 Percent",
+      threshold: 30
+    });
+
+    // CloudFormation stack outputs for easy lookup
+    new CfnOutput(this, 'DbCpuMetricName', {
+      value: dbCpuMetric.metricName
+    });
+    new CfnOutput(this, 'DbWriterCpuMetricName', {
+      value: dbWriterCpuMetric.metricName
+    });
+    new CfnOutput(this, 'DbReaderCpuMetricName', {
+      value: dbReaderCpuMetric.metricName
+    });
+
+    new CfnOutput(this, 'DbCpuAlarmName', {
+      value: dbCpuAlarm.alarmName
+    });
+    new CfnOutput(this, 'DbWriterCpuAlarmName', {
+      value: dbWriterCpuAlarm.alarmName
+    });
+    new CfnOutput(this, 'DbReaderCpuAlarmName', {
+      value: dbReaderCpuAlarm.alarmName
+    });
+
+    new CfnOutput(this, 'DbWriterEndpoint', {
+      value: dbCluster.clusterEndpoint.socketAddress
+    });
+    new CfnOutput(this, 'DbReaderEndpoint', {
+      value: dbCluster.clusterReadEndpoint.socketAddress
+    });
   }
 }
